1dcc751f171f416605789646bd5479ff76c2730b
Lionel <lionel.zoubritzky@gmail.com>
2022-03-08 11:42:57 +0100

    Deploy documentation

--- docs/make.jl
+++ docs/make.jl
@@ -43,6 +43,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/coudertlab/CrystalNets.jl.git"
+)
