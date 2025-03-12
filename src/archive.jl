0a88a898b3560524f7b99ad2eb23ebccad6a5c29
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2025-03-12 03:42:37 +0100

    Update archive version to 3

--- src/archive.jl
+++ src/archive.jl
@@ -1,7 +1,7 @@
 ## Handling of the topological archive internally used to recognize topologies.
 using Pkg.Artifacts
 
-const CRYSTALNETS_ARCHIVE_VERSION = "2.5"
+const CRYSTALNETS_ARCHIVE_VERSION = "3"
 const arc_location = artifact"archives"
 
 """
